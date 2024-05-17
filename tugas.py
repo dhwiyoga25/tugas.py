5fb65de1a8bbbd225b988a7a6cc74e5e3806b7d7
dhwiyoga25 <yogamd260@gmail.com>
2024-05-17 19:52:56 +0700

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -1,3 +1,4 @@
+#jawaban soal 1
 nama = "Yoga Dwi Sutarto"
 nim = "20220801284"
 mata_kuliah = "Machine Learning"
@@ -6,19 +7,24 @@ print("Nama : ", nama)
 print("nim: ", nim)
 print("Perkuliahan: ", mata_kuliah)
 
+#jawaban soal 2
 matriksC= [[1,2,3],[4,5,6]]
 print(matriksC)
 
+#jawaban soal 3
 matriks = [[2,2],[2,2],[2,2]]
 print(matriks)
 
+#jawaban soal 4
 import numpy as np
 
+#jawaban soal 5
 from numpy import *
 matriks = range(32)
 matriks = reshape(matriks, (4,8))
 print(matriks)
 
+#jawaban soal 6
 import numpy as np
 from numpy import *
 matriks = np.random.randint(1,5,(3,30))
