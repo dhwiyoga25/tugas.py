deb8ca278e0931335e8cf377ffae44cf972e3477
dhwiyoga25 <yogamd260@gmail.com>
2024-05-17 19:38:36 +0700

    Create tugas.py

---
+++ tugas.py
@@ -0,0 +1,25 @@
+nama = "Yoga Dwi Sutarto"
+nim = "20220801284"
+mata_kuliah = "Machine Learning"
+print("DATA DIRI")
+print("Nama : ", nama)
+print("nim: ", nim)
+print("Perkuliahan: ", mata_kuliah)
+
+matriksC= [[1,2,3],[4,5,6]]
+print(matriksC)
+
+matriks = [[2,2],[2,2],[2,2]]
+print(matriks)
+
+import numpy as np
+
+from numpy import *
+matriks = range(32)
+matriks = reshape(matriks, (4,8))
+print(matriks)
+
+import numpy as np
+from numpy import *
+matriks = np.random.randint(1,5,(3,30))
+print(matriks)
